fa273c82a734d8046bd4d1ebceeb1c7c5dea3b85
jalajakshi3012 <jalajakshi3012@gmail.com>
2024-01-16 12:56:42 +0530

    DSA Algorithms2

---
+++ CurrencyDenominator/src/com/gl/denominator/Main.java
@@ -0,0 +1,17 @@
+package com.gl.denominator;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+CurrencyDenominations currencyDenominations=new CurrencyDenominations();
+		
+		currencyDenominations.CollectCurrencyList();
+		currencyDenominations.CollectAmount();
+		currencyDenominations.CalculateDenominations();
+
+	}
+
+}
